ca8ba106f2a5bc9dc288341d2d7bae12130b11c4
Mathavaroopan <roopanmathava@gmail.com>
2025-03-05 10:12:36 +0530

    trying default vite.config.js again

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "react-router-dom": "/node_modules/react-router-dom/",
-    },
-  },
-  build: {
-    rollupOptions: {
-      external: ["react-router-dom"],
-    },
-  },
-});
+})
